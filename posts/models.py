5698831352ff0f5cde618641efa5415a1ca9b9f9
khawla-Moh <khawlaMoh00@gmail.com>
2023-11-05 13:40:39 +0300

    design post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+   post:
+       -title
+       -content
+       -draft
+       -publish_date
+       -author
+       -image
+       -tags
+       -category
+       -comments
+
+'''
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=20000)
+    draft=models.BooleanField(default=True)
