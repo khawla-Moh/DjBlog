c20bbb0c3f32d9b6741ac52a425ae30d3c343d09
khawla-Moh <khawlaMoh00@gmail.com>
2023-11-07 19:53:15 +0300

    add ctegory

--- posts/models.py
+++ posts/models.py
@@ -25,8 +25,15 @@ class Post(models.Model):
     publish_date=models.DateTimeField()    
     tags = TaggableManager() 
     image=models.ImageField(upload_to='post')
+    category=models.ForeignKey('Category',related_name='post_category',on_delete=models.SET_NULL,null=True)
     """ publish_date2=models.DateTimeField(auto_now=True)
      """
     
     def __str__(self):
         return self.title 
+    
+class Category(models.Model):
+    name=models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.name   
