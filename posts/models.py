05d97f0fc9fdab52ee1a95e85b0b6f94a97c7c41
khawla-Moh <khawlaMoh00@gmail.com>
2023-11-07 16:33:19 +0300

    add static and media setting

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.utils.timezone import now
-
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -22,6 +21,10 @@ class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=20000)
     draft=models.BooleanField(default=True)
-    publish_date=models.DateTimeField(default=now, editable=False)
+    publish_date=models.DateTimeField()    
+    tags = TaggableManager() 
+    """ publish_date2=models.DateTimeField(auto_now=True)
+     """
+    
     def __str__(self):
         return self.title 
