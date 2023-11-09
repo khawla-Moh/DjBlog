147e02f752ee5c6108f4b090e1b14a605f273dd3
khawla-Moh <khawlaMoh00@gmail.com>
2023-11-10 00:14:18 +0300

    add poat_list view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+
+    data=Post.objects.all( )  #from db get all posts
+
+    context={
+        'khawlah':data
+    }
+    return render(request,'posts/post_list.html',context)
+
+
+def post_details():
+    pass
