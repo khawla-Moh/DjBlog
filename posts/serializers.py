a65aefcf2f3758f3fe3ca882f8be6415fdc00f95
khawla-Moh <khawlaMoh00@gmail.com>
2023-11-28 22:27:11 +0300

    .

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth.models import User
 
-
+class UserSerilaizers(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id','username','email']
 
 
 class PostSerilaizers(serializers.ModelSerializer):
+    author=UserSerilaizers()
+    category=serializers.StringRelatedField()
     class Meta:
         model=Post              #form for model(Post)
         fields='__all__'         #all columns  appear in form 
